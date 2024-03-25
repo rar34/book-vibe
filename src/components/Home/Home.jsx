e3c567fe0ce4f56439b21a4eb6c29f071d4d59af
rar34 <ra.cse34@gmail.com>
2024-03-25 21:33:24 +0600

    added navbar

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../NavBar/NavBar";
+
+const Home = () => {
+    return (
+        <div className="max-w-6xl mx-auto">
+            <NavBar></NavBar>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+import Home from "../Home/Home";
+
+const Root = () => {
+    return (
+        <div>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Root;
